67413908a91b90b8952c0809529e81d0c7cef3d9
Phúc Nguyễn <94228115+0899656100phuc@users.noreply.github.com>
2021-11-16 08:45:33 +0700

    upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo Đạm! 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Em yêu anh nhiều không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em yêu  Anh nhiều đó nha :> ',
+    btnYes: 'Có, em thật sự Yêu anh 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em Yêu anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai + với đá bóng giỏi đó Ahiiiii 😊😊',
+    mess: 'Anh biết mà, Anh cũng yêu em lắm Moa... 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/profile.php?id=100009059714554'
+}
